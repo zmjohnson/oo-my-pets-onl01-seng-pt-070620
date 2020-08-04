b1c57c39f23c44352149ec2657fcb1c56d4f2f89
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-04 19:29:28 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood
+    @mood
+  end
 end

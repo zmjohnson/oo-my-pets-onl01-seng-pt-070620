16c75ba7b383c9a7cf7733010ed05fcc75842cc4
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-04 19:27:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,26 +1,14 @@
 class Owner
+  # code goes here
   attr_accessor :name, :pets
   attr_reader :species
 
-@@owners = []
+  @@owners = []
 
-  def initialize(name)
-    @name = name
-    @species = "human"
+  def initialize(species, pets = {:fishes => [], :dogs => [], :cats => []})
+    @species = species
+    @pets = pets
     @@owners << self
-    @pets = {fishes: [], dogs: [], cats: []}
-  end
-
-  def self.all
-    @@owners
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def say_species
@@ -28,46 +16,58 @@ class Owner
   end
 
   def buy_fish(fish_name)
-    pets[:fishes] << Fish.new(fish_name)
+    @pets[:fishes] << Fish.new(fish_name)
   end
 
   def buy_cat(cat_name)
-    pets[:cats] << Cat.new(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
   end
 
   def buy_dog(dog_name)
-    pets[:dogs] << Dog.new(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
   end
 
   def walk_dogs
-    pets[:dogs].each do |dog|
+    @pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    pets[:cats].each do |cat|
+    @pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    pets[:fishes].each do |fish|
+    @pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
 
   def sell_pets
-    pets.each do |species, animals|
-      animals.each do |animal|
-        animal.mood = "nervous"
-        end
-      animals.clear
+    @pets.each do |type, name_array|
+      name_array.each do |pet|
+        pet.mood = "nervous"
+        #name_array.delete(pet)
+      end 
     end
+    @pets = {}
   end
 
   def list_pets
-    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
-end
+
+  def self.all
+    @@owners
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def self. count
+    @@owners.length
+  end
 end
